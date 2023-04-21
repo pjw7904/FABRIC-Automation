fc8bcff240fedf48671772e05595d2b83f99ad20
Peter Willis <pjw7904@rit.edu>
2023-04-21 18:25:17 +0000

    Fix config generator spacing error

--- remote_scripts/config_generator.sh
+++ remote_scripts/config_generator.sh
@@ -5,8 +5,8 @@ sudo rm -f mtp_dcn.conf
 sudo touch mtp_dcn.conf
 
 # isTor:[true/false] | isTopSpine:[true/false] | tier:[1-N] | startHelloTime:[epoch_timestamp] | topEthPortName:[ethX]
-echo "isTor: $1" > mtp_dcn.conf
-echo "isTopSpine: $2" >> mtp_dcn.conf
-echo "tier: $3" >> mtp_dcn.conf
-echo "startHelloTime: $4" >> mtp_dcn.conf
-echo "torEthPortName: $5" >> mtp_dcn.conf
+echo "isTor:$1" > mtp_dcn.conf
+echo "isTopSpine:$2" >> mtp_dcn.conf
+echo "tier:$3" >> mtp_dcn.conf
+echo "startHelloTime:$4" >> mtp_dcn.conf
+echo "torEthPortName:$5" >> mtp_dcn.conf
