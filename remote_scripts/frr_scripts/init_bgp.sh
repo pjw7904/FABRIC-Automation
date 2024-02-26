8f55f7111a7c1aebab182d54aa64479d14cdf135
Peter Willis <pjw7904@rit.edu>
2024-02-26 19:11:00 +0000

    Add script for init BGP DCN topologies

---
+++ remote_scripts/frr_scripts/init_bgp.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# frr-stable will be the latest official stable release. This can be changed if you're looking for a specific version of FRR.
+FRRVER="frr-stable"
+
+# Add RPM repository. This was built for Red Hed 8, but works with Rocky 8 as well.
+#    Note: Supported since FRR 7.3
+curl -O https://rpm.frrouting.org/repo/$FRRVER-repo-1-0.el8.noarch.rpm
+sudo yum install -y ./$FRRVER*
+
+# install FRR.
+sudo yum install -y frr frr-pythontools
+
+# Give permissions to user to access frr files (this requires a logout after to take effect)
+sudo usermod -a -G frr,frrvty $(logname)
+
+# Install tshark for packet-level inspections and tmux for multiplexing capabilties if necessary.
+sudo dnf install -y tmux wireshark
+
+# Turn on IP forwarding
+sudo sysctl -w net.ipv4.ip_forward=1
+
+# A "here" document to get around the logout/login requirements of adding a group to a user
+newgrp frr << END
+sudo sed -i 's/bgpd=no/bgpd=yes/g' /etc/frr/daemons
+sudo sed -i 's/#frr_profile="datacenter"/frr_profile="datacenter"/g' /etc/frr/daemons
+sudo service frr start
+END
+
+echo "Installation complete!"
