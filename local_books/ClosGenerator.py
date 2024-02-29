c3042e244dc790a0d150c1e4f9bf997d01ed39d3
Peter Willis <pjw7904@rit.edu>
2024-02-29 15:52:19 +0000

    Add BGP DCN extension to clos generator

--- local_books/ClosGenerator.py
+++ local_books/ClosGenerator.py
@@ -5,6 +5,7 @@ Desc: Class to help build Clos topologies and the node attributes required.
 
 import networkx as nx
 from copy import deepcopy
+from ipaddress import ip_address, IPv4Address, IPv4Network
 
 class ClosGenerator:
     # Vertex prefixes to denote position in topology
@@ -69,9 +70,9 @@ class ClosGenerator:
 
         return title
        
-    def generateNodeName(self, prefix, nodeNum, currentTier, topTier):
+    def generateNode(self, prefix, nodeNum, currentTier, topTier):
         """
-        Determine what a given node should be named. The format of a name is node_title-pod_prefix-num for all nodes minus the top tier, which do not use a pod_prefix.
+        Determine what a given node should be named and create it. The format of a name is node_title-pod_prefix-num for all nodes minus the top tier, which do not use a pod_prefix.
 
         :param prefix: The prefix for a given tier within a pod in the topology.
         :param nodeNum: A number associated with that specific node.
@@ -116,7 +117,7 @@ class ClosGenerator:
         return
 
     
-    def addConnectionToGraph(self, northNode, southNode, northTier, southTier):
+    def connectNodes(self, northNode, southNode, northTier, southTier, **kwargs):
         """
         Connect two nodes together via an edge. The nodes must be in adjacent tiers (ex: tier 2 and tier 3). The nodes also understand if their new neighbor is above them (northbound) or below them (southbound). Subclasses specific to a protocol should override this method with its specific attribute needs beyond north-south interconnection. This base method is provided to simply view the output of a given folded-Clos topology.
 
@@ -132,6 +133,8 @@ class ClosGenerator:
 
         self.clos.nodes[northNode]["southbound"].append(southNode)
         self.clos.nodes[southNode]["northbound"].append(northNode)
+        
+        # self.addProtocolConfig(**kwargs)
 
         self.clos.add_edge(northNode, southNode)
 
@@ -161,10 +164,11 @@ class ClosGenerator:
 
         while currentTierPrefix:
             currentPrefix = currentTierPrefix.pop(0)
+            print(f"Current prefix: {currentPrefix}")
             nodeNum = 0 # The number associated with a given node, appended after the prefix (ex: 1-1-1, pod 1-1, node number 1)
 
             for node in range(1,currentPodNodes+1):
-                northNode = self.generateNodeName(currentPrefix, str(node), currentTier, topTier)
+                northNode = self.generateNode(currentPrefix, str(node), currentTier, topTier)
 
                 for intf in range(1, southboundPorts+1):
                     # Per BFS logic, mark the neighbor as visited if it has not already and add it to the queue.
@@ -186,9 +190,9 @@ class ClosGenerator:
                         southPrefix = northNode.strip(self.LEAF_NAME)
                         southNodeNum = intf
 
-                    southNode = self.generateNodeName(southPrefix, str(southNodeNum), currentTier-1, topTier)
+                    southNode = self.generateNode(southPrefix, str(southNodeNum), currentTier-1, topTier)
 
-                    self.addConnectionToGraph(northNode, southNode, currentTier, currentTier-1)
+                    self.connectNodes(northNode, southNode, currentTier, currentTier-1)
 
                 nodeNum += 1
 
@@ -249,5 +253,168 @@ class ClosGenerator:
                     
                     for s in self.clos.nodes[node]["southbound"]:
                         logFile.write("\t\t{}\n".format(s))
+                        
+        return
+
+
+class BGPDCNConfig(ClosGenerator):
+    PROTOCOL = "BGP"
+    PRIVATE_ASN_RANGE_START = 64512
+
+    LEAF_SPINE_SUPERNET = '172.16.0.0/12'
+    COMPUTE_SUPERNET = '192.168.0.0/16'
+    LEAF_SPINE_SUBNET_BITS = 12
+    COMPUTE_SUBNET_BITS = 8
+
+    def __init__(self, k, t, name):
+        """
+        Initializes a graph and its data structures to hold network information.
+
+        :param k: Degree shared by each node.
+        :param t: Number of tiers in the graph.
+        :param name: The name you want to give the topology.
+        """
+
+        # Call superclass constructor to get graph setup
+        super().__init__(k, t, name)
+
+        # Configure how BGP will assign ASNs and IPv4 addressing
+        self.ASNAssignment = {None : None}
+        self.currentASN = self.PRIVATE_ASN_RANGE_START
+        self.IPAssignment = {}
+
+        self.coreNetworks = list(IPv4Network(self.LEAF_SPINE_SUPERNET).subnets(prefixlen_diff=self.LEAF_SPINE_SUBNET_BITS))
+        self.edgeNetworks = list(IPv4Network(self.COMPUTE_SUPERNET).subnets(prefixlen_diff=self.COMPUTE_SUBNET_BITS))
+        
+    def generateNode(self, prefix, nodeNum, currentTier, topTier):
+        """
+        Determine what a given node should be named and create it. The format of a name is node_title-pod_prefix-num for all nodes minus the top tier, which do not use a pod_prefix.
+
+        :param prefix: The prefix for a given tier within a pod in the topology.
+        :param nodeNum: A number associated with that specific node.
+        :param currentTier: The folded-Clos tier that is being anaylzed currently.
+        :param topTier: The folded-Clos tier at the top of the topology.
+        :returns: The name given to the node.
+        """
+
+        title = self.getNodeTitle(currentTier, topTier)
+
+        if(currentTier == topTier):
+            partialName = title + "-"
+        else:
+            partialName = title + prefix + "-"
+
+        # Add the unique number given to this node in the pod.
+        name = partialName + nodeNum
+            
+        # ASN stuff
+        ASNPrefix = None
+
+        # Compute nodes don't get an ASN
+        if(title != self.COMPUTE_NAME):
+            # Every leaf gets its own ASN
+            if(title == self.LEAF_NAME):
+                ASNPrefix = name
+
+            # Spines in a pod get the same ASN
+            elif(title == self.TOF_NAME or title == self.SPINE_NAME):
+                ASNPrefix = partialName
+
+            if(ASNPrefix not in self.ASNAssignment):
+                self.ASNAssignment[ASNPrefix] = self.currentASN
+                self.currentASN += 1
+
+        # I'm not sure if the conditional is needed here in a new function, but it was used successfully in the base class
+        if(name not in self.clos):
+            self.clos.add_node(name, northbound=[], southbound=[], tier=None, ASN=self.ASNAssignment[ASNPrefix], ipv4={}, advertise=[])
+
+        # Return just the name, not the node object iself
+        return name
+   
+    # Have the origional add **kwags so you can add the prefix?
+    def connectNodes(self, northNode, southNode, northTier, southTier, **kwargs):
+        """
+        Connect two nodes together via an edge. Also configure the BGP ASN number and IP addressing.
+
+        :param northNode: The node in tier N.
+        :param southNode: The node in tier N-1.
+        :param northTier: The tier value N.
+        :param southTier: The tier value N-1.
+        """
+
+        NEXT_SUBNET = 0
+
+        # If one of the nodes is a compute node, this is an edge network.
+        if(southTier == self.COMPUTE_TIER):
+            subnet = list(self.edgeNetworks.pop(NEXT_SUBNET))[:-1] # Remove broadcast address
+        else:
+            subnet = list(self.coreNetworks.pop(NEXT_SUBNET))[1:-1] # Remove network and broadcast address
+
+        # Assign IP addressing
+        if(northTier == self.LEAF_TIER):
+            networkAddress = subnet.pop(0)
+            northAddress = subnet.pop()
+            self.clos.nodes[northNode]["advertise"].append(f"{networkAddress}/24")
+        else:
+            northAddress = subnet.pop(0)
+
+        southAddress = subnet.pop(0)
+
+        self.clos.nodes[northNode]["southbound"].append(southNode)
+        self.clos.nodes[northNode]["tier"] = northTier
+        self.clos.nodes[northNode]["ipv4"][southNode] = str(northAddress)
+
+        self.clos.nodes[southNode]["northbound"].append(northNode)
+        self.clos.nodes[southNode]["tier"] = southTier
+        self.clos.nodes[southNode]["ipv4"][northNode] = str(southAddress)
+
+        self.clos.add_edge(northNode, southNode)
+
+        return
+
+    def logGraphInfo(self, k, t, topTier):
+        """
+        Output folded-Clos topology information into a log file.
+        
+        :param k: Degree shared by each node.
+        :param t: Number of tiers in the graph.
+        :param topTier: The folded-Clos tier at the top of the topology.
+        """
+
+        numTofNodes = (k//2)**(t-1)
+        numServers = 2*((k//2)**t)
+        numSwitches = ((2*t)-1)*((k//2)**(t-1))
+        numLeaves = 2*((k//2)**(t-1))
+        numPods = 2*((k//2)**(t-2))
+        
+        with open(f'{k}_{t}_BGP.log', 'w') as logFile:
+            logFile.write("=============\nFOLDED CLOS\nk = {k}, t = {t}\n{k}-port devices with {t} tiers.\n=============\n".format(k=k, t=t))
+
+            logFile.write("Number of ToF Nodes: {}\n".format(numTofNodes))
+            logFile.write("Number of physical servers: {}\n".format(numServers))
+            logFile.write("Number of networking nodes: {}\n".format(numSwitches))
+            logFile.write("Number of leaves: {}\n".format(numLeaves))
+            logFile.write("Number of Pods: {}\n".format(numPods))
+
+            for tier in reversed(range(topTier+1)):
+                nodes = [v for v in self.clos if self.clos.nodes[v]["tier"] == tier]
+                logFile.write("\n== TIER {} ==\n".format(tier))
+
+                for node in sorted(nodes):
+                    logFile.write(node)
+                    logFile.write(f'\n\tASN = {self.clos.nodes[node]["ASN"]}') # BGP ASN printout
+                    logFile.write(f'\n\tAdvertised routes: {self.clos.nodes[node]["advertise"]}')
+                    logFile.write("\n\tnorthbound:\n")
+                    
+                    for n in self.clos.nodes[node]["northbound"]:
+                        addr = self.clos.nodes[node]["ipv4"][n]
+                        logFile.write(f"\t\t{n} - {addr}\n")
+                        
+                    logFile.write("\n\tsouthbound:\n")
+                    
+                    for s in self.clos.nodes[node]["southbound"]:
+                        #logFile.write("\t\t{}\n".format(s))
+                        addr = self.clos.nodes[node]["ipv4"][s]
+                        logFile.write(f"\t\t{s} - {addr}\n")
                         
         return
