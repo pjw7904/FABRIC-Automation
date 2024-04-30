a4e691e4d9968beaeedfae0c1ad75f443ee5158f
Peter Willis <pjw7904@rit.edu>
2024-04-30 16:55:12 +0000

    Update intf down timestamp to match FRR

--- remote_scripts/bgp_scripts/intf_down.sh
+++ remote_scripts/bgp_scripts/intf_down.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Mark the time
-date +"%D %T.%3N" > /home/rocky/bgp_scripts/intf_down.log
+date +"%Y/%m/%d %H:%M:%S.%3N" > /home/rocky/bgp_scripts/intf_down.log
 
 # Take the interface down
 sudo ip link set dev $1 down
