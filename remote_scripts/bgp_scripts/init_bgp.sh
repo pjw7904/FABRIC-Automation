d21a4df9baa51f43c5f3ccab64e01b93bf94ff88
Peter Willis <pjw7904@rit.edu>
2025-07-18 01:40:02 +0000

    Fix bug where the FRR package randomly fails to download

--- remote_scripts/bgp_scripts/init_bgp.sh
+++ remote_scripts/bgp_scripts/init_bgp.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
+# Stop on the first error if something goes wrong.
+set -euo pipefail
+
 # frr-stable will be the latest official stable release. This can be changed if you're looking for a specific version of FRR.
 FRRVER="frr-stable"
+REPO_RPM="https://rpm.frrouting.org/repo/${FRRVER}-repo.el8.noarch.rpm"
 
-# Add RPM repository. This was built for Red Hed 8, but works with Rocky 8 as well.
-#    Note: Supported since FRR 7.3
-curl -O https://rpm.frrouting.org/repo/$FRRVER-repo-1-0.el8.noarch.rpm
-sudo yum install -y ./$FRRVER*
-
-# install FRR.
-sudo yum install -y frr frr-pythontools
+curl -fL --retry 5 --retry-connrefused --retry-delay 2 -o /tmp/frr-repo.rpm "$REPO_RPM"
+sudo dnf -y install /tmp/frr-repo.rpm
+sudo dnf -y install frr frr-pythontools
 
 # Give permissions to user to access frr files (this requires a logout after to take effect)
 sudo usermod -a -G frr,frrvty $(logname)
@@ -26,7 +26,7 @@ newgrp frr << END
 sudo sed -i 's/bgpd=no/bgpd=yes/g' /etc/frr/daemons
 sudo sed -i 's/bfdd=no/bfdd=yes/g' /etc/frr/daemons
 sudo sed -i 's/#frr_profile="datacenter"/frr_profile="datacenter"/g' /etc/frr/daemons
-sudo mv bgp_scripts/frr.conf /etc/frr/frr.conf
+sudo cp bgp_scripts/frr.conf /etc/frr/frr.conf
 sudo service frr start
 END
 
