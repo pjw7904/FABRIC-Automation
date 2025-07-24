08c3b0b31b04aaad5fc88e85a5de0fa4ef9c2428
Peter Willis <pjw7904@rit.edu>
2025-07-23 23:51:49 -0400

    Update MTP experiment analysis for hard and soft link failures

--- local_books/mtp/MTP_Analysis.py
+++ local_books/mtp/MTP_Analysis.py
@@ -9,36 +9,107 @@
 # Iterate through each valid file in the given directory to determine metric results
 
 # %%
-LOG_DIR_PATH = "../logs/mtp/log_file_test2"
+LOG_DIR_PATH = "/home/pjw7904/fabric/FABRIC-Automation/local_books/mtp/MTP_logs/mtp_soft_failure/test_5"
 NETWORK_NODE_PREFIXES = "T,S,L"
 COMPUTE_NODE_PREFIXES = "C"
+EXPERIMENT_LOG_FILE = "experiment.log"
+DEBUGGING = False
 
 # %%
+from pathlib import Path
+
+def getResultsFile(metric_directory, includeNodeName=False):
+    directory_path = Path(LOG_DIR_PATH) / metric_directory
+
+    for file_path in directory_path.iterdir():
+        if not file_path.is_file():
+            continue
+
+        node_name = file_path.stem.split("_", 1)[0]
+
+        if(includeNodeName):
+            yield str(file_path), node_name
+        else:
+            yield str(file_path)
+
+
+# %%
+import re
 import os
 
-def getResultsFile(metricDirectory):
-    directoryPath = os.path.join(LOG_DIR_PATH, metricDirectory)
+HARD_LINK_FAILURE = 1
+SOFT_LINK_FAILURE = 2
 
-    for fileName in os.listdir(directoryPath):
-        filePath = os.path.join(directoryPath, fileName)
+failedNodes = set()
 
-        # We want to avoid this directory, as it has nothing to do with the test, but will show up regardless.
-        if(".ipynb" not in fileName):
-            yield filePath
-    
-    return
+# Iterate through each logEntry in the file and store the data
+experimentLogFile = os.path.join(LOG_DIR_PATH, EXPERIMENT_LOG_FILE)
+with open(experimentLogFile) as file:
+    for line in file:
+        line = line.strip()
+
+        if line.startswith("Failed node:"):
+            NODE_TO_FAIL = line.split(":", 1)[1].strip()
+            failedNodes.add(NODE_TO_FAIL)
+
+        elif line.startswith("Failed neighbor:"):
+            NEIGHBOR_TO_FAIL = line.split(":", 1)[1].strip()
+            failedNodes.add(NEIGHBOR_TO_FAIL)
+
+        elif line.startswith("Interface name:"):
+            NODE_INTF_NAME = line.split(":", 1)[1].strip()
+
+        elif line.startswith("Neighbor interface name:"):
+            NEIGHBOR_INTF_NAME = line.split(":", 1)[1].strip()
+
+        elif line.startswith("Experiment type:"):
+            match = re.match(r"Experiment type: (.+?) link failure", line)
+            if match:
+                    failureType = match.group(1)
+                    EXPERIMENT_TYPE = SOFT_LINK_FAILURE if failureType.strip() == "soft" else HARD_LINK_FAILURE
+            else:
+                raise Exception("Unknown failure type.")
 
 # %% [markdown]
-# ## <span style="color: #034694"><b>Determine interface failure and test stop timestamps</b></span>
+# ## <span style="color: #034694"><b>Determine experiment timing bounds</b></span>
 
 # %%
-def getIntfDownTime(logFile):
+from datetime import datetime, timezone
+
+# Experiment timing log files
+START_TIME_FILE_NAME = "intf_down.log"
+NODES_DOWN_FILE_NAME = "nodes_down.log"
+
+# Experiment timing log sudirectory
+DOWNTIME_DIR = "downtime"
+
+# Format for interface-down timestamp
+TIMESTAMP_FORMAT = "%Y/%m/%d %H:%M:%S.%f" # Example timestamp in this format: 2024/04/30 04:09:33.947
+
+
+def getEpochTime(timeString, timestampFormat=TIMESTAMP_FORMAT):
     '''
-    Read the first line of the interface downtime file, which must be the time in epoch formatting.
+    Return a epoch (unix) timestamp based on a standard timestamp. Keeps everything in UTC.
     '''
 
-    with open(logFile) as file:
-        return int(file.readline())
+    datetimeFormat = datetime.strptime(timeString, timestampFormat).replace(tzinfo=timezone.utc)
+    return int(datetimeFormat.timestamp() * 1000)
+
+
+def getStartTime(intfDownFile, startTimestamp, startTimeFormatted):
+    '''
+    Record the start time, which is the time the interface was disabled.
+    '''
+
+    with open(intfDownFile) as file:
+        candidateStartTime = file.readline().rstrip()
+        canidateStartTimeEpoch = getEpochTime(candidateStartTime)
+
+    if(startTimestamp == 0 or (canidateStartTimeEpoch < startTimestamp)):
+        startTimestamp = canidateStartTimeEpoch
+        startTimeFormatted = candidateStartTime
+
+    return startTimestamp, startTimeFormatted
 
 
 def getTestStopTime(logFile):
@@ -56,16 +127,87 @@ def getTestStopTime(logFile):
     
     return testStopTime
 
+
+startTimestamp = 0 # Time when the interface went down.
+startTimeFormatted = "" # Standard clock time
+
 # Analyze each file in the downtime subdirectory
-for logFile in getResultsFile("downtime"):
-    if("nodes_down" in logFile): # Determine what the test stoptime was.
+for logFile in getResultsFile(DOWNTIME_DIR):
+    # Determine what the test stoptime was
+    if(NODES_DOWN_FILE_NAME in logFile): 
         testStopTime = getTestStopTime(logFile)
-    elif("intf_down" in logFile): # Determine when the interface failure occurred.
-        failureTime = getIntfDownTime(logFile)
+
+    # Determine when the first interface failure occurred
+    elif(START_TIME_FILE_NAME in logFile): 
+        startTimestamp, startTimeFormatted = getStartTime(logFile, startTimestamp, startTimeFormatted)
+
+    # No idea what this file is, might want to check that out
     else:
         print(f"Unknown file {logFile}")
 
-print(f"Interface failure timestamp: {failureTime}\nTest stop timestamp: {testStopTime}")
+if(not startTimestamp):
+    raise Exception(f"No start time found, please check the log directory {LOG_DIR_PATH}.")
+
+print(f"Down/Start time: {startTimeFormatted} (epoch: {startTimestamp})")
+print(f"Test stop time (epoch): {testStopTime}")
+
+#print(f"Interface failure timestamp: {failureTime}\nTest stop timestamp: {testStopTime}")
+
+# %% [markdown]
+# ## <span style="color: #034694"><b>Convergence Time</b></span>
+
+# %%
+MTP_LOG_FILE_NAME = "mtp.log"
+
+UPDATE_LOG_STATEMENT = "FAILURE UPDATE message received"
+
+def getNodeConvergenceTime(logFile):
+    '''
+    For a given node/log file, determine when the node reached convergence.
+    '''
+
+    nodeConvergenceTime = 0
+
+    with open(logFile) as file:
+        logEntry = file.readline()
+    
+        while logEntry:
+            if(UPDATE_LOG_STATEMENT in logEntry):
+                token = logEntry.split(" ")
+                entryTime = int(token[6].replace(",",""))
+    
+                if(entryTime < testStopTime):
+                    if DEBUGGING: print(f"\t{logEntry.rstrip()}")
+                    # Find the farthest valid entry from the start of logging.
+                    nodeConvergenceTime = max(nodeConvergenceTime, entryTime)
+            
+            logEntry = file.readline()
+        
+    return nodeConvergenceTime
+
+# Default starting values for the timing range
+failureDetectionTimestamp = -1
+finalFailureRecoveryTimestamp = -1
+
+# Store all of the node's convergence time's.
+convergenceTimes = []
+
+for logFile in getResultsFile("convergence"):
+    if(MTP_LOG_FILE_NAME in logFile):
+        # Add each node's convergence time to the convergence times list
+        convergenceTimes.append(getNodeConvergenceTime(logFile))
+
+if(not convergenceTimes):
+    raise Exception(f"No node convergence times found, please check the log directory {LOG_DIR_PATH}.")
+
+# Find the change that occured last out of all changes to finalizes network-wide reconvergence.
+lastChangeTimestamp = max(convergenceTimes)
+
+# Determine reconvergence time
+reconvergenceTime = lastChangeTimestamp - startTimestamp
+
+print(f"\nDown/Start time: {startTimeFormatted}")
+print(f"Reconvergence time: {reconvergenceTime} milliseconds")
 
 # %% [markdown]
 # ## <span style="color: #034694"><b>Control Overhead & Blast Radius</b></span>
@@ -86,16 +228,16 @@ def parseOverhead(line, timestamp):
     return overhead
 
 def getOverhead(logFile):
-    with open(logFile) as f:
-        line = f.readline()
+    with open(logFile) as file:
+        logEntry = file.readline()
         size = 0
     
-        while line:
-            if "FAILURE UPDATE message received" in line:
-                time = parseTimestamp(line)
-                size += parseOverhead(f.readline(), time)
+        while logEntry:
+            if UPDATE_LOG_STATEMENT in logEntry:
+                time = parseTimestamp(logEntry)
+                size += parseOverhead(file.readline(), time)
     
-            line = f.readline()
+            logEntry = file.readline()
 
     return size
 
@@ -104,23 +246,20 @@ totalOverhead = 0
 
 # Blast Radius values
 totalNodeCount = 0
-effectedNodeCount = 2 # Starts at 2 for the 2 nodes that lost a link. They won't have any updates.
+effectedNodeCount = 0
 
-for logFile in getResultsFile("convergence"):
-    # Blast radius value updates
-    totalNodeCount += 1
-    
-    if(os.path.getsize(logFile) > 0):
+for logFile, nodeName in getResultsFile("convergence", includeNodeName=True):
+    if(MTP_LOG_FILE_NAME in logFile):
         nodeOverhead = getOverhead(logFile)
 
-        if(nodeOverhead > 0):
+        # Blast radius value updates
+        totalNodeCount += 1
+        if(nodeOverhead > 0 or nodeName in failedNodes):
             effectedNodeCount += 1
 
         # Add node's overhead to total overhead
         totalOverhead += nodeOverhead
-    
-    else:
-        print(f"Log file {logFile} is empty")
+        
 
 # Calculate blast radius as the fraction of total nodes that received updates
 blastRadius = (effectedNodeCount/totalNodeCount) * 100
@@ -129,53 +268,5 @@ blastRadius = (effectedNodeCount/totalNodeCount) * 100
 print(f"Overhead: {totalOverhead} bytes")
 print(f"\nBlast radius: {blastRadius:.2f}% of nodes received VID failure information.")
 print(f"\tNodes receiving updated information: {effectedNodeCount}\n\tTotal nodes: {totalNodeCount}")
-
-# %% [markdown]
-# ## <span style="color: #034694"><b>Convergence Time</b></span>
-
-# %%
-# Types values for failure actions
-FAILURE_DETECTION = 0
-FAILURE_UPDATE = 1
-
-def getNodeConvergenceTime(logFile):
-    with open(logFile) as f:
-        line = f.readline()
-    
-        while line:
-            token = line.split(" ")
-            
-            if "Detected a failure" in line:            
-                return 0,int(token[len(token)-1])
-            
-            elif "FAILURE UPDATE message received" in line:
-                time = int(token[6].replace(",",""))
-    
-                if(time < testStopTime):
-                    return 1,int(time)
-            
-            line = f.readline()
-        
-    return -1,-1
-
-# Default starting values for the timing range
-failureDetectionTimestamp = -1
-finalFailureRecoveryTimestamp = -1
-
-for logFile in getResultsFile("convergence"):
-    if(os.path.getsize(logFile) > 0):
-        failureType, failureTimestamp = getNodeConvergenceTime(logFile)
-
-        if(failureType == FAILURE_DETECTION):
-            if(failureDetectionTimestamp > 0):
-                raise Exception("Multiple begin times, please check log")
-            failureDetectionTimestamp = failureTimestamp
-
-        elif(failureType == FAILURE_UPDATE):
-            finalFailureRecoveryTimestamp = max(finalFailureRecoveryTimestamp, failureTimestamp)
-
-convergenceTime = finalFailureRecoveryTimestamp - failureDetectionTimestamp
-print(f"Final failure update time: {finalFailureRecoveryTimestamp}\nFailure Detection time: {failureDetectionTimestamp}")
-print(f"Convergence time: {convergenceTime} ms")
 
 
