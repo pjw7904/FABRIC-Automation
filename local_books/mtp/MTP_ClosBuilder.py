eb410fe40b8bfdff430baac4c25a21d20eb2023b
Peter Willis <pjw7904@rit.edu>
2026-01-10 14:05:05 -0500

    Update MTP Clos builder to remove extra post-boot config.

--- local_books/mtp/MTP_ClosBuilder.py
+++ local_books/mtp/MTP_ClosBuilder.py
@@ -1,8 +1,23 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.18.1
+#   kernelspec:
+#     display_name: fabric
+#     language: python
+#     name: python3
+# ---
+
 # %% [markdown]
 # # <span style="color: #034694"><b>MTP</b></span> Folded-Clos Builder
-# 
+#
 # This book provides the following actions:
-# 
+#
 # 1. Building a folded-Clos topology.
 # 2. Downloading the MTP implementation as the network control-plane and data-plane on the folded-Clos topology.
 # 3. Configuring IPv4 addressing on all devices in the compute/server subnets in the topology. This includes the compute devices themselves and the leaf interfaces connected to these subnets.
@@ -10,14 +25,14 @@
 
 # %% [markdown]
 # ## <span style="color: #034694"><b>Input Required Information</b></span>
-# 
+#
 # ### <span style="color: #034694"><b>FABRIC Configuration</b></span> 
 # | Variable | Use |
 # | :-- | --- |
 # | SLICE_NAME| Name of slice you want to create. Please make sure a slice with that name does not already exist. |
 # | SITE_NAME| Name of the FABRIC site you want the nodes to be reserved at. This code does not consider inter-site situations, the entire topology is reserved on a single slice. |
 # | MEAS_ADD      | Enter True if measurements are to be taken on the slice. This requires the inclusion of a separate measurement node. If you don't understand how to use the Measurement Framework, don't set this to true. |
-# 
+#
 # ### <span style="color: #034694"><b>Folded-Clos Configuration</b></span>
 # | Variable | Use |
 # | :-- | --- |
@@ -32,13 +47,11 @@
 # %%
 # FABRIC Configuration
 SLICE_NAME = "clos_mtp"
-SITE_NAME = "WASH"
-MEAS_ADD = False
-SEC_ADD = True
+SITE_NAME = "MASS"
 
 # Folded-Clos Configuration
 PORTS_PER_DEVICE = 4
-NUMBER_OF_TIERS = 2
+NUMBER_OF_TIERS = 3
 NETWORK_NODE_PREFIXES = "T,S,L"
 COMPUTE_NODE_PREFIXES = "C"
 SOUTHBOUND_PORT_DENSITY = {1:1}
@@ -65,7 +78,7 @@ except Exception as e:
 
 # %% [markdown]
 # ## <span style="color: #034694"><b>Build a Graph of the Folded-Clos Topology</b></span> 
-# 
+#
 # A custom library, ClosGenerator, is used to build a graph-representation of a folded-Clos topology. MTP configuration is added as attributes to the nodes as the graph is being constructed.
 
 # %%
@@ -82,7 +95,7 @@ print(f"Folded-Clos topology details (Not considering port density changes):\n{t
 
 # %% [markdown]
 # ## <span style="color: #034694"><b>Prepare the MTP Configuration Template</b></span> 
-# 
+#
 # This book uses the Mako template engine to populate MTP-related information into the MTP configuration file (mtp.conf). The per-node MTP configuration is contained in the graph structure built in the prior section. 
 
 # %%
@@ -99,45 +112,13 @@ except Exception as e:
 # ## <span style="color: #034694"><b>Parse the folded-Clos Configuration and Create the Slice</b></span> 
 
 # %%
-import os
-
-# CONFIGURATION FOR MTP-SPEAKING LEAF AND SPINE NODES
-def addMTPConfiguration(node, nodeInfo, topology, mtpTemplate):
-    '''
-    Prepare a node for the MTP implementation to be utilized.
-    '''
-
-    # Store tier information
-    nodeTemplate = {'tier': topology.getNodeAttribute(node, 'tier'), 'isTopSpine': topology.getNodeAttribute(node, 'isTopTier')}
-
-    # Process the stored information and render a custom mtp.conf.
-    nodeMTPData = mtpTemplate.render(**nodeTemplate)
-
-    # Add FABRIC post-boot tasks to get the node ready for FRR installation
-    nodeInfo.add_post_boot_upload_directory(MTP_SCRIPTS_LOCATION,'.')
-    nodeInfo.add_post_boot_execute(f'sudo echo -e "{nodeMTPData}" > mtp_scripts/mtp.conf')
-    nodeInfo.add_post_boot_execute('sudo chmod +x /home/rocky/mtp_scripts/*.sh')
-
-    return
-
-# CONFIGURATION FOR NON-MTP-SPEAKING COMPUTE NODES
-def addComputeConfiguration(nodeInfo):
-    '''
-    Prepare a node for traffic testing.
-    '''
-    
-    nodeInfo.add_post_boot_upload_directory(MTP_SCRIPTS_LOCATION,'.')
-    nodeInfo.add_post_boot_execute('sudo chmod +x /home/rocky/mtp_scripts/*.sh')
-
-    return
-
 # Create the slice
 slice = fablib.new_slice(name=SLICE_NAME)
 
 addedNodes = {} # Visited nodes structure, format = name : nodeInfo
 
 # Add slice-specific information to the log file
-logFile.update({"name": SLICE_NAME, "site": SITE_NAME, "meas": MEAS_ADD})
+logFile.update({"name": SLICE_NAME, "site": SITE_NAME})
 
 # Iterate over each network in the topology and configure each interface connected to the network, and the network itself.
 for networkInfo in topology.iterNetwork(fabricFormating=True):
@@ -153,12 +134,6 @@ for networkInfo in topology.iterNetwork(fabricFormating=True):
         if(node not in addedNodes):
             # Add the node to the FABRIC slice.
             nodeInfo = slice.add_node(name=node, cores=1, ram=4, image='default_rocky_8', site=SITE_NAME)
-
-            # If the node is a non-compute node, it needs MTP configuration instructions.
-            if(topology.isNetworkNode(node)):
-                addMTPConfiguration(node, nodeInfo, topology, mtpTemplate)
-            else:
-                addComputeConfiguration(nodeInfo)
 
             addedNodes[node] = nodeInfo
             print(f"\tAdded node {node} to the slice.")
@@ -176,61 +151,10 @@ for networkInfo in topology.iterNetwork(fabricFormating=True):
     print(f"\tAdded network {network}")
 
 # %% [markdown]
-# ## <span style="color: #034694"><b>Add a Hacker Node (Optional)</b></span> 
-
-# %%
-ATTACHED_NODE = "T-1" # The first top-tier node is the default node to attach the hacker to the topology.
-HACKER_NODE = "H-1"
-HACKER_NETWORK = (HACKER_NODE, ATTACHED_NODE)
-HACKER_NETWORK_TYPE = "edge" # Not a core network, an edge network (or is it? Whatever you want it to be)
-HACKER_NODE_TIER = -1
-
-if(SEC_ADD):
-    # Add the hacker node.
-    hackerInfo = slice.add_node(name=HACKER_NODE, cores=4, ram=4, disk=80, image='default_debian_11', site=SITE_NAME)
-    addComputeConfiguration(hackerInfo)
-    
-    # Create interfaces for the new top-tier node + hacker network.
-    attachedNodeIntfName = f"intf-{HACKER_NODE}"
-    attachedNodeIntf = addedNodes[ATTACHED_NODE].add_component(model='NIC_Basic', name=attachedNodeIntfName).get_interfaces()[0]
-    
-    hackerNodeIntfName = f"intf-{ATTACHED_NODE}"
-    hackerNodeIntf = hackerInfo.add_component(model='NIC_Basic', name=hackerNodeIntfName).get_interfaces()[0]
-    
-    # Add the network to the FABRIC slice.
-    networkName = f"edge-{HACKER_NODE}-{ATTACHED_NODE}"
-    slice.add_l2network(name=networkName, interfaces=[hackerNodeIntf, attachedNodeIntf], type="L2Bridge")
-    print("Added hacker node and network")
-
-    # Add it to the log file
-    logFile.update({f"tier_{HACKER_NODE_TIER}": {HACKER_NODE: {"northbound" : [], "southbound" : [ATTACHED_NODE]}}})
-
-else:
-    print("No hacker node added.")
-
-# %% [markdown]
-# ## <span style="color: #034694"><b>Add a Measurement Node (Optional)</b></span> 
-
-# %%
-if(MEAS_ADD):
-    import mflib 
-    print(f"MFLib version  {mflib.__version__} " )
-
-    from mflib.mflib import MFLib
-
-    # Add measurement node to topology using static method.
-    MFLib.addMeasNode(slice, disk=100, image='docker_ubuntu_20', site=SITE_NAME)
-    print("Measurement node added.")
-
-else:
-    print("No measurement node added.")
-
-# %% [markdown]
 # ## <span style="color: #034694"><b>Submit the Slice</b></span>
 
 # %%
-%%time
-import json 
+# %%time
 
 try:
     # Submit Slice Request
@@ -243,24 +167,10 @@ except Exception as e:
     traceback.print_exc()
 
 # %% [markdown]
-# ## <span style="color: #034694"><b>Initalize the Measurement Framework (Optional)</b></span>
-# 
-# This step both initalizes and instrumentizes the Measurement framework to use Prometheus and Grafana. If ELK is desired, modifications to the cell need to be made.
-
-# %%
-if(MEAS_ADD):
-    mf = MFLib(SLICE_NAME) # Initalize
-    instrumetize_results = mf.instrumentize( ["prometheus"] ) # Instrumentize
-    
-    # Grafana SSH Tunnel Command
-    print(mf.grafana_tunnel)
-    print(f"Browse to https://localhost:{mf.grafana_tunnel_local_port}/grafana/dashboards?query=%2A")
-
-# %% [markdown]
 # ## <span style="color: #034694"><b>Provide Initial Configuration to Nodes</b></span> 
-# 
+#
 # If the node is a core (MTP-speaking) node, it will have the MTP implementation downloaded and compiled.
-# 
+#
 # If the node is an edge (non-MTP-speaking) node, it will have the traffic generator code installed.
 
 # %%
@@ -271,6 +181,33 @@ try:
     
 except Exception as e:
     print(f"Exception: {e}")
+
+
+# %%
+# CONFIGURATION FOR MTP-SPEAKING LEAF AND SPINE NODES
+def addMTPConfiguration(node, topology, mtpTemplate):
+    '''
+    Prepare a node for the MTP implementation to be utilized.
+    '''
+
+    # Store tier information
+    nodeTemplate = {'tier': topology.getNodeAttribute(node, 'tier'), 'isTopSpine': topology.getNodeAttribute(node, 'isTopTier')}
+
+    # Process the stored information and render a custom mtp.conf.
+    nodeMTPData = mtpTemplate.render(**nodeTemplate)
+
+    # Perform the transfer to the node-specific configuration file
+    manager.executeCommandsParallel(f'sudo echo -e "{nodeMTPData}" > mtp_scripts/mtp.conf', prefixList=node)
+
+    return
+
+# Upload the scripts directory for all nodes (switches + servers) and provide the correct permissions
+manager.uploadDirectoryParallel(MTP_SCRIPTS_LOCATION)
+manager.executeCommandsParallel('sudo chmod +x /home/rocky/mtp_scripts/*.sh')
+
+# Configure the switches
+for node in topology.iterNodes(noComputeNodes=True):
+    addMTPConfiguration(node, topology, mtpTemplate)
 
 # %%
 # Commands to execute the bash scripts configuring the nodes
@@ -285,9 +222,9 @@ manager.executeCommandsParallel(edgeNodeConfig, prefixList=COMPUTE_NODE_PREFIXES
 
 # %% [markdown]
 # ## <span style="color: #034694"><b>Add IPv4 Addressing to Compute Subnet Nodes</b></span> 
-# 
+#
 # This system utilizes the addressing provided by the ClosGenerator module:
-# 
+#
 # * 192.168.0.0/16 is the compute supernet. All compute subnets are given a /24 subnet. Compute devices are given lower addresses (ex: .1) and the leaf node is given a high address (ex: .254)
 
 # %%
@@ -304,7 +241,7 @@ for node in topology.iterNodes():
     for neighbor, currentAddress in topology.getNodeAttribute(node, 'ipv4').items():
         # Access the interface from FABRIC.
         intfName = f"{node}-intf-{neighbor}-p1" # Naming is a bit strange, but is formatted in FABRIC as such.
-        intf = slice.get_interface(intfName)
+        intf = manager.slice.get_interface(intfName)
 
         # Convert the address and subnet into ipaddress objects for FABRIC processing.
         fabAddress = IPv4Address(currentAddress)
@@ -335,12 +272,9 @@ for nodeName in topology.iterNodes():
     tierNumber = topology.getNodeAttribute(nodeName, 'tier')
     logFile[f"tier_{tierNumber}"][nodeName]["ssh"] = manager.slice.get_node(nodeName).get_ssh_command()
 
-# Add the hacker node as well if it is present
-if(SEC_ADD):
-    logFile[f"tier_{HACKER_NODE_TIER}"][HACKER_NODE]["ssh"] = manager.slice.get_node(HACKER_NODE).get_ssh_command()
 
 # %%
+import json 
+
 with open(f'{SLICE_NAME}_k{PORTS_PER_DEVICE}_t{NUMBER_OF_TIERS}_MTP.json', "w") as outfile:
     json.dump(logFile, outfile)
-
-
